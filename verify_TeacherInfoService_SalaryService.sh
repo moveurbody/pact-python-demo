9567dcd18eaedf833572903f726d4b9847c5ab1d
yu_hsuan_chen <yu_hsuan_chen@trend.com.tw>
2019-12-30 14:26:04 +0800

    add salary service

---
+++ verify_TeacherInfoService_SalaryService.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+VERSION=$1
+if [ -x $VERSION ]; then
+    echo "ERROR: You must specify a provider version"
+    exit
+fi
+
+pipenv run pact-verifier --provider-base-url=http://localhost:8001 \
+  --pact-url="http://127.0.0.1/pacts/provider/TeacherInfoService/consumer/Salary/latest" \
+  --provider-states-setup-url=http://localhost:8001/api/teachers/init \
+  --provider-app-version $VERSION \
+  --pact-broker-username pactbroker \
+  --pact-broker-password pactbroker \
+  --publish-verification-results
