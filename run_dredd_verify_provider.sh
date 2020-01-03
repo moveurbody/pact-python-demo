6407fcbf20db150dc5462d41962ca39ec2fb90c1
yu_hsuan_chen <yu_hsuan_chen@trend.com.tw>
2020-01-03 16:34:54 +0800

    add script for dredd and prism

---
+++ run_dredd_verify_provider.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dredd ./demo_services/swagger_files/teacher_swagger.yml http://127.0.0.1:8001
---
+++ run_prism_verify_consumer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+prism mock ./demo_services/swagger_files/teacher_swagger.json -p 8001
